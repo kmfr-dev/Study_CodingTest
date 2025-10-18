ea5675610a4f137a9f02c31be245298294c20f89
kmfr-dev <zksekrwl1@naver.com>
2025-10-18 19:52:37 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 4404 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,17 +1,18 @@
 #include <iostream>
-#include <vector>
 
+// 입력 가능한 최대 수
 const int MaxNumber = 10000;
 
 int main()
 {
-	// 입력 받을 케이스를 선언한다.
+	// 입력 개수
 	int Case = { 0 };
-	// 입력을 받는다.
 	std::cin >> Case;
 	
+	// 각 숫자의 등장 횟수를 저장할 배열
 	int Arr[MaxNumber] = { 0 };
 	
+	// 입력된 숫자들의 개수를 센다
 	for (int i = 0; i < Case; ++i)
 	{
 		int Number = { 0 };
@@ -19,10 +20,11 @@ int main()
 		Arr[Number - 1] += 1;
 	};
 
+	// 등장 횟수만큼 숫자를 오름차순으로 출력
+	 // std::endl 대신 개행으로 출력 속도 최적화
 	for (int i = 0; i < MaxNumber; ++i)
 	{
 		for (int j = 0; j < Arr[i]; ++j)
 			std::cout << i + 1 << '\n';
 	}
-	
 }
